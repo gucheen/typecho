b588f3f2de7175ec389b683004f455c9105be595
Cheng Gu <guchengf@gmail.com>
2020-09-01 09:12:02 +0800

    update(markdown): replace with parsedown

--- var/Markdown.php
+++ var/Markdown.php
@@ -21,16 +21,10 @@ class Markdown
         static $parser;
 
         if (empty($parser)) {
-            $parser = new HyperDown();
-
-            $parser->hook('afterParseCode', function ($html) {
-                return preg_replace("/<code class=\"([_a-z0-9-]+)\">/i", "<code class=\"lang-\\1\">", $html);
-            });
-
-            $parser->enableHtml(true);
+            $parser = new Parsedown();
         }
 
-        return str_replace('<p><!--more--></p>', '<!--more-->', $parser->makeHtml($text));
+        return str_replace('<p><!--more--></p>', '<!--more-->', $parser->text($text));
     }
 
     /**
